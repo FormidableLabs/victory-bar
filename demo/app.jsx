da1a1e607b46366e358a6cc61fc9640b5a1ac2d4
Colin Megill <colinmegill@gmail.com>
2015-09-10 03:27:45 -0700

    converted all sections of code over, understand bostocks magic

--- demo/app.jsx
+++ demo/app.jsx
@@ -1,11 +1,13 @@
 /*global document:false*/
 import React from "react";
 import {VictoryBar} from "../src/index";
+import {data} from "./data"
 
 class App extends React.Component {
   render() {
     return (
-        <VictoryBar/>
+        <VictoryBar
+          data={data}/>
     );
   }
 }
--- src/components/victory-bar.jsx
+++ src/components/victory-bar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Radium from "radium";
-
+import lodash from "lodash";
 
 @Radium
 class VictoryBar extends React.Component {
@@ -18,14 +18,74 @@ class VictoryBar extends React.Component {
   //   };
   // }
 
+  drawStackedBars () {
+
+    // make a copy so we don't mutate props
+    let localCopyOfData = _.cloneDeep(this.props.data);
+
+    var color = d3.scale.ordinal().range(
+      ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]
+    ).domain(legend);
+
+    localCopyOfData.forEach((bar) => {
+      var y0 = 0;
+      bar.segments = color.domain().map((segmentName) => {
+        return {
+          segmentName: segmentName,
+          y0: y0,
+          y1: y0 += +bar[segmentName]
+        };
+      });
+      bar.total = bar.ages[bar.ages.length - 1].y1;
+    });
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1)
+        .domain(localCopyOfData.map((bar) => {
+          return bar.label;
+        }));
+
+    var y = d3.scale.linear()
+        .rangeRound([height, 0])
+        .domain([0, d3.max(localCopyOfData, (bar) => {
+          return bar.total;
+        })]);
+
+    // localCopyOfData.sort((a, b) => {
+    //    return b.total - a.total;
+    // });
+
+    function makeSegments (segments, i) {
+      var barSvg = segments.map((segment) => {
+        return (
+          <rect
+            width={x.rangeBand()}
+            height={y(segment.y0) - y(segment.y1)}
+            y={y(segment.y1)} />
+        )
+      })
+      return barSvg;
+    })
+
+    const stackedBars = dataWithSegmentsAdded.map((bar, i) => {
+      return (
+        <g tranform={"translate(" + x(bar.label) + ",0)"}>
+          {makeSegments(bar.segments)}
+        </g>
+      )
+    })
+    return stackedBars;
+
+  }
+
   drawBars() {
     const bars = this.props.data.map((d, i) => {
       return (
-        <rect
-          width={20}
-          height={d}
-          x={i * 30}
-          y={this.props.height - d} />
+          <rect
+            width={20}
+            height={d}
+            x={i * 30}
+            y={this.props.height - d} />
       )
     })
     return bars;
@@ -36,7 +96,7 @@ class VictoryBar extends React.Component {
     return (
       <svg width={this.props.width} height={this.props.height}>
         <g>
-          {this.drawBars()}
+          {_.isArray(this.props.data[0]) ? this.drawStackedBars() : this.drawBars()}
         </g>
       </svg>
     );
