28106b78bae6142ceb13ad58da3e4c7d67dab8b4
Lauren <lauren.eastridge@formidablelabs.com>
2016-01-15 16:52:38 -0800

    use helper methods, remove values stored on this

--- src/components/victory-bar.jsx
+++ src/components/victory-bar.jsx
@@ -216,342 +216,13 @@ export default class VictoryBar extends React.Component {
     width: 450
   };
 
-  getCalculatedValues(props) {
-    this.style = this.getStyles(props);
-    this.padding = this.getPadding(props);
-    this.stringMap = {
-      x: this.createStringMap(props, "x"),
-      y: this.createStringMap(props, "y")
-    };
-    this.datasets = this.consolidateData(props);
-    this.range = {
-      x: this.getRange(props, "x"),
-      y: this.getRange(props, "y")
-    };
-    this.domain = {
-      x: this.getDomain(props, "x"),
-      y: this.getDomain(props, "y")
-    };
-    this.scale = {
-      x: this.getScale(props, "x"),
-      y: this.getScale(props, "y")
-    };
-  }
 
-  getStyles(props) {
-    const style = props.style || defaultStyles;
-    const {data, labels, parent} = style;
-    return {
-      parent: _.merge({height: props.height, width: props.width}, parent),
-      data: _.merge({}, defaultStyles.data, data),
-      labels: _.merge({}, defaultStyles.labels, labels)
-    };
-  }
-
-  getPadding(props) {
-    const padding = _.isNumber(props.padding) ? props.padding : 0;
-    const paddingObj = _.isObject(props.padding) ? props.padding : {};
-    return {
-      top: paddingObj.top || padding,
-      bottom: paddingObj.bottom || padding,
-      left: paddingObj.left || padding,
-      right: paddingObj.right || padding
-    };
-  }
-
-  consolidateData(props) {
-    const dataFromProps = _.isArray(props.data[0]) ? props.data : [props.data];
-    return _.map(dataFromProps, (dataset, index) => {
-      return {
-        attrs: this.getAttributes(props, index),
-        data: _.map(dataset, (data) => {
-          return _.merge(data, {
-            // determine category if it exists
-            category: this.determineCategoryIndex(data.x, props.categories),
-            // map string data to numeric values, and add names
-            x: _.isString(data.x) ? this.stringMap.x[data.x] : data.x,
-            xName: _.isString(data.x) ? data.x : undefined,
-            y: _.isString(data.y) ? this.stringMap.y[data.y] : data.y,
-            yName: _.isString(data.y) ? data.y : undefined
-          });
-        })
-      };
-    });
-  }
-
-  determineCategoryIndex(x, categories) {
-    // if categories don't exist or are not given as an array of arrays, return undefined;
-    if (!categories || !_.isArray(categories[0])) {
-      return undefined;
-    }
-    // determine which range band this x value belongs to, and return the index of that range band.
-    return categories.findIndex((category) => {
-      return (x >= Math.min(...category) && x <= Math.max(...category));
-    });
-  }
-
-  getAttributes(props, index) {
-    let attributes = props.dataAttributes && props.dataAttributes[index] ?
-      props.dataAttributes[index] : props.dataAttributes;
-    if (attributes) {
-      attributes.fill = attributes.fill || this.getColor(props, index);
-    } else {
-      attributes = {fill: this.getColor(props, index)};
-    }
-    const requiredAttributes = {
-      name: attributes && attributes.name ? attributes.name : `data-${index}`
-    };
-    return _.merge(requiredAttributes, attributes);
-  }
-
-  getColor(props, index) {
-    // check for styles first
-    if (props.style && props.style.data && props.style.data.fill) {
-      return props.style.data.fill;
-    }
-    const colorScale = _.isArray(props.colorScale) ?
-      props.colorScale : Util.Style.getColorScale(props.colorScale);
-    return colorScale[index % colorScale.length];
-  }
-
-  createStringMap(props, axis) {
-    // if categories exist and are strings, create a map using only those strings
-    // don't alter the order.
-    if (props.categories && Util.Collection.containsStrings(props.categories)) {
-      return _.zipObject(_.map(props.categories, (tick, index) => {
-        return [`${tick}`, index + 1];
-      }));
-    }
-    // collect strings from data
-    const data = _.isArray(props.data) ? _.flattenDeep(props.data) : props.data;
-    // create a unique, sorted set of strings
-    const stringData = _.chain(data)
-      .pluck(axis)
-      .map((datum) => {
-        return _.isString(datum) ? datum : null;
-      })
-      .compact()
-      .uniq()
-      .sort()
-      .value();
-
-    return _.isEmpty(stringData) ?
-      null :
-      _.zipObject(_.map(stringData, (string, index) => {
-        return [string, index + 1];
-      }));
-  }
-
-  getScale(props, axis) {
-    let scale;
-    if (props.scale && props.scale[axis]) {
-      scale = props.scale[axis].copy();
-    } else if (props.scale && !_.isObject(props.scale)) {
-      scale = props.scale.copy();
-    } else {
-      scale = d3Scale.linear().copy();
-    }
-    scale.range(this.range[axis]);
-    scale.domain(this.domain[axis]);
-    return scale;
-  }
-
-  getRange(props, axis) {
-    // determine how to lay the axis and what direction positive and negative are
-    const isVertical =
-      !this.props.horizontal && axis === "x" || this.props.horizontal && axis !== "x";
-
-    return isVertical ? [this.padding.left, props.width - this.padding.right] :
-      [props.height - this.padding.bottom, this.padding.top];
-  }
-
-  getDomain(props, axis) {
-    let domain;
-    const categoryDomain = this.getDomainFromCategories(props, axis);
-    if (props.domain && props.domain[axis]) {
-      domain = props.domain[axis];
-    } else if (props.domain && _.isArray(props.domain)) {
-      domain = props.domain;
-    } else if (categoryDomain) {
-      domain = categoryDomain;
-    } else {
-      domain = this.getDomainFromData(props, axis);
-    }
-    return this.padDomain(domain, axis);
-  }
-
-  padDomain(domain, axis) {
-    let domainPadding;
-    if (this.props.domainPadding[axis]) {
-      domainPadding = this.props.domainPadding[axis];
-    } else {
-      domainPadding = _.isNumber(this.props.domainPadding) ? this.props.domainPadding : 0;
-    }
-    if (domainPadding === 0) {
-      return domain;
-    }
-    const domainMin = Math.min(...domain);
-    const domainMax = Math.max(...domain);
-    const rangeExtent = Math.abs(Math.max(...this.range[axis]) - Math.min(...this.range[axis]));
-    const extent = Math.abs(domainMax - domainMin);
-    const percentPadding = domainPadding / rangeExtent;
-    const padding = extent * percentPadding;
-    // don't make the axes cross if they aren't already
-    const adjustedMin = (domainMin >= 0 && (domainMin - padding) <= 0) ?
-      0 : domainMin.valueOf() - padding;
-    const adjustedMax = (domainMax <= 0 && (domainMax + padding) >= 0) ?
-      0 : domainMax.valueOf() + padding;
-    return _.isDate(domainMin) || _.isDate(domainMax) ?
-      [new Date(adjustedMin), new Date(adjustedMax)] : [adjustedMin, adjustedMax];
-  }
-
-  getDomainFromCategories(props, axis) {
-    if (axis !== "x" || !props.categories || Util.Collection.containsStrings(props.categories)) {
-      return undefined;
-    }
-    const categories = _.flatten(props.categories);
-    return [Math.min(...categories), Math.max(...categories)];
-  }
-
-  getDomainFromData(props, axis) {
-    // find the global min and max
-    const datasets = this.datasets.map((dataset) => dataset.data);
-    const axisData = _.flatten(datasets).map((data) => data[axis]);
-    const globalMin = Math.min(...axisData);
-    const globalMax = Math.max(...axisData);
-
-    // find the cumulative max for stacked chart types
-    // this is only sensible for the y domain
-    // TODO check assumption
-    const cumulativeData = (props.stacked && axis === "y" && this.datasets.length > 1) ?
-      this.getCumulativeData(datasets, "y") : [];
-
-    const cumulativeMaxArray = cumulativeData.map((dataset) => {
-      return dataset.reduce((memo, val) => {
-        return val > 0 ? memo + val : memo;
-      }, 0);
-    });
-
-    const cumulativeMinArray = cumulativeData.map((dataset) => {
-      return dataset.reduce((memo, val) => {
-        return val < 0 ? memo + val : memo;
-      }, 0);
-    });
-
-    const cumulativeMin = Math.min(...cumulativeMinArray);
-    // use greatest min / max
-    const domainMin = cumulativeMin < 0 ? cumulativeMin : globalMin;
-    const domainMax = Math.max(globalMax, Math.max(...cumulativeMaxArray));
-    return [domainMin, domainMax];
-  }
-
-  getCumulativeData(datasets, axis) {
-    const categories = [];
-    const xValues = [];
-    datasets.forEach((dataset) => {
-      dataset.forEach((data) => {
-        if (data.category !== undefined && !_.includes(categories, data.category)) {
-          categories.push(data.category);
-        } else if (!_.includes(xValues, data.x)) {
-          xValues.push(data.x);
-        }
-      });
-    });
-
-    const dataByCategory = () => {
-      return categories.map((value) => {
-        const categoryData = datasets.filter((data) => data.category === value);
-        return _.flatten(categoryData.map((data) => data[axis]));
-      });
-    };
-
-    const dataByIndex = () => {
-      return xValues.map((value, index) => {
-        return datasets.map((data) => data[index] && data[index][axis]);
-      });
-    };
-
-    return _.isEmpty(categories) ? dataByIndex() : dataByCategory();
-  }
-
-  pixelsToValue(pixels, axis) {
-    const domainExtent = Math.max(...this.domain[axis]) - Math.min(...this.domain[axis]);
-    const rangeExtent = Math.max(...this.range[axis]) - Math.min(...this.range[axis]);
-    return domainExtent / rangeExtent * pixels;
-  }
-
-  adjustX(data, index, options) {
-    const x = data.x;
-    const stacked = options && options.stacked;
-    const center = this.datasets.length % 2 === 0 ?
-      this.datasets.length / 2 : (this.datasets.length - 1) / 2;
-    const centerOffset = index - center;
-    const totalWidth = this.pixelsToValue(this.style.data.padding, "x") +
-      this.pixelsToValue(this.style.data.width, "x");
-    if (data.category !== undefined) {
-      // if this is category data, shift x to the center of its category
-      const rangeBand = this.props.categories[data.category];
-      const bandCenter = (Math.max(...rangeBand) + Math.min(...rangeBand)) / 2;
-      return stacked ? bandCenter : bandCenter + (centerOffset * totalWidth);
-    }
-    return stacked ? x : x + (centerOffset * totalWidth);
-  }
-
-  getYOffset(data, index, barIndex) {
-    const minY = Math.min(...this.domain.y);
-    if (index === 0) {
-      return Math.max(minY, 0);
-    }
-    const y = data.y;
-    const previousDataSets = _.take(this.datasets, index);
-    const previousBars = _.map(previousDataSets, (dataset) => {
-      return _.pluck(dataset.data, "y");
-    });
-    return _.reduce(previousBars, (memo, bar) => {
-      const barValue = bar[barIndex];
-      const sameSign = (y < 0 && barValue < 0) || (y >= 0 && barValue >= 0);
-      return sameSign ? memo + barValue : memo;
-    }, 0);
-  }
-
-  getLabelIndex(data) {
-    if (data.category !== undefined) {
-      return data.category;
-    } else if (this.stringMap.x) {
-      return (data.x - 1);
-    } else {
-      const allX = this.datasets.map((dataset) => {
-        return dataset.data.map((datum) => datum.x);
-      });
-      const uniqueX = _.uniq(_.flatten(allX));
-      return (_.findIndex(_.sortBy(uniqueX), (n) => n === data.x));
-    }
-  }
-
-  getBarPosition(data, index, barIndex) {
-    const stacked = this.props.stacked;
-    const yOffset = this.getYOffset(data, index, barIndex);
-    const y0 = stacked ? yOffset : Math.max(Math.min(...this.domain.y), 0);
-    const y1 = stacked ? yOffset + data.y : data.y;
-    const x = this.adjustX(data, index, {stacked});
-    return {
-      independent: this.scale.x.call(this, x),
-      dependent0: this.scale.y.call(this, y0),
-      dependent1: this.scale.y.call(this, y1)
-    };
-  }
-
-  renderBars(dataset, index) {
-    const isCenter = Math.floor(this.datasets.length / 2) === index;
-    const isLast = this.datasets.length === index + 1;
-    const stacked = this.props.stacked;
-    const plotGroupLabel = (stacked && isLast) || (!stacked && isCenter);
-    return _.map(dataset.data, (data, barIndex) => {
-      const position = this.getBarPosition(data, index, barIndex);
-      const styleData = _.omit(data, [
-        "xName", "yName", "x", "y", "label", "category"
-      ]);
-      const style = _.merge({}, this.style.data, _.omit(dataset.attrs, "name"), styleData);
+  renderBars(dataset, seriesIndex, calculatedProps) {
+    return dataset.data.map((data, barIndex) => {
+      const index = {seriesIndex, barIndex};
+      const position = LayoutHelpers.getBarPosition(data, index, calculatedProps);
+      const baseStyle = calculatedProps.style;
+      const style = LayoutHelpers.getBarStyle(data, dataset, baseStyle);
       const barComponent = (
         <Bar key={`series-${index}-bar-${barIndex}`}
           horizontal={this.props.horizontal}
@@ -560,9 +231,8 @@ export default class VictoryBar extends React.Component {
           data={data}
         />
       );
-      const plotLabel = (plotGroupLabel && (this.props.labels || this.props.labelComponents));
-      if (plotLabel) {
-        const labelIndex = this.getLabelIndex(data);
+      if (LayoutHelpers.shouldPlotLabel(seriesIndex, this.props, calculatedProps.datasets)) {
+        const labelIndex = LayoutHelpers.getLabelIndex(data, calculatedProps);
         const labelText = this.props.labels ?
           this.props.labels[labelIndex] || this.props.labels[0] : "";
         const labelComponent = this.props.labelComponents ?
@@ -572,7 +242,7 @@ export default class VictoryBar extends React.Component {
             {barComponent}
             <BarLabel key={`label-series-${index}-bar-${barIndex}`}
               horizontal={this.props.horizontal}
-              style={this.style.labels}
+              style={baseStyle.labels}
               position={position}
               data={data}
               labelText={labelText}
@@ -585,9 +255,32 @@ export default class VictoryBar extends React.Component {
     });
   }
 
-  renderData() {
-    return _.map(this.datasets, (dataset, index) => {
-      return this.renderBars(dataset, index);
+  renderData(props, style) {
+    const datasets = Data.consolidateData(props);
+    const stringMap = {
+      x: Data.createStringMap(props, "x"),
+      y: Data.createStringMap(props, "y")
+    };
+    const padding = Chart.getPadding(props);
+    const range = {
+      x: Chart.getRange(props, "x"),
+      y: Chart.getRange(props, "y")
+    };
+    const domain = {
+      x: DomainHelpers.getDomain(props, "x"),
+      y: DomainHelpers.getDomain(props, "y")
+    };
+    const scale = {
+      x: Scale.getBaseScale(props, "x").domain(domain.x).range(range.x),
+      y: Scale.getBaseScale(props, "y").domain(domain.y).range(range.y)
+    };
+    const stacked = props.stacked;
+    const categories = props.categories;
+    const calculatedProps = {
+      categories, datasets, domain, padding, range, scale, stacked, stringMap, style
+    };
+    return datasets.map((dataset, index) => {
+      return this.renderBars(dataset, index, calculatedProps);
     });
   }
 
@@ -608,11 +301,9 @@ export default class VictoryBar extends React.Component {
           {(props) => <VictoryBar {...this.props} {...props} animate={null}/>}
         </VictoryAnimation>
       );
-    } else {
-      this.getCalculatedValues(this.props);
     }
-    const style = this.style.parent;
-    const group = <g style={style}>{this.renderData()}</g>;
-    return this.props.standalone ? <svg style={style}>{group}</svg> : group;
+    const style = Chart.getStyles(this.props, defaultStyles);
+    const group = <g style={style.parent}>{this.renderData(this.props, style)}</g>;
+    return this.props.standalone ? <svg style={style.parent}>{group}</svg> : group;
   }
 }
